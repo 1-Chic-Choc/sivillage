6127810d3f24173dc247d863330ae5cb95975f2e
oror-sine <1staries@naver.com>
2024-09-18 00:57:26 +0900

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter, Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Footer from "@/components/footer/Footer";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -30,14 +31,17 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="ko"
       className={cn(
         roboto.className,
         roboto.variable,
         regularBoldCello.variable,
       )}
     >
-      <body className={cn(roboto.className)}>{children}</body>
+      <body className={cn(roboto.className)}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
