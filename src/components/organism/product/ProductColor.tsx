a97aa22f68638b627c5709761358aaf2d41633c5
oror-sine <1staries@naver.com>
2024-09-28 20:48:03 +0900

    feat: 다중 필터링 구현 및 일부 개선 보안

--- src/components/organism/product/ProductColor.tsx
+++ src/components/organism/product/ProductColor.tsx
@@ -1,10 +1,21 @@
 import { getColor } from "@/action/productAction";
+import { colorImageMap } from "@/datas/colorImageData";
+import Image from "next/image";
 
 interface ProductColorProps {
   colorId: number;
 }
 
 export default async function ProductColor({ colorId }: ProductColorProps) {
-  const color = await getColor({ id: colorId });
-  return <div>{color?.name}</div>;
+  const color = (await getColor({ id: colorId }))?.name || "기타";
+  const src = colorImageMap.get(color);
+  return (
+    <Image
+      src={src!}
+      alt={color}
+      width={16}
+      height={16}
+      className="border-[0.6667px] border-black"
+    />
+  );
 }
